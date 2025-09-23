6a982b7ff687b86ac9563e41f3dad84ed869b003
vivekggu <vivekggu9569@gmail.com>
2025-09-23 04:04:32 +0000

    conversionof datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score)); 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33" => 33
+// "333abc" => Nan
+// true => 1; false =>0 
+
+let isLoggedIn = "vivek"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true ; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
