867ef98eff7f95c7fb65d8169338896e3f5aff5e
vivekggu <vivekggu9569@gmail.com>
2025-09-23 03:33:34 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat all js code as newer version
+
+// alert (3+3) // we are using nodejs not browser
+
+console.log(3+3)
+
+console.log("vivek")
+
+let name = "vivek"
+let age = 16
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint
+// string =>
+// boolean =>true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "vivek")
